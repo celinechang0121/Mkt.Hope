35ff16ea5843abfcba93fd1ee8ca1342a1e13246
Celine Chang <celine.chang.28@Celines-MacBook-Air.local>
2022-12-28 13:11:25 +0900

    Initial commit

---
+++ app/src/main/java/celine/chang/mkthope/ReceiveMenu.java
@@ -0,0 +1,14 @@
+package celine.chang.mkthope;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class ReceiveMenu extends AppCompatActivity {
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.receive_menu);
+        }
+}
